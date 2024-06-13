739e90b776bc3387fdaf1c85c9c9f1bf92a8f260
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-06-13 15:03:38 +0200

    Create InitParking method, class Vehicle, subclasses
    - Class Vehicle
    - Subclass Car, Boat, Airplan, Bus, Motorcycle

--- GarageApplication/Program.cs
+++ GarageApplication/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace GarageApplication
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using static GarageApplication.Program.Car;
+
+namespace GarageApplication
 {
     internal class Program
     {
@@ -29,7 +33,9 @@
                     case '1':
                         Console.Clear();
                         Console.WriteLine("User wants to PARK");
-                        //ExamineList();
+                        var parkVehicle = new ParkVehicle();
+                        parkVehicle.InitParking();
+                        
                         break;
                     case '2':
                         Console.Clear();
@@ -53,7 +59,89 @@
                         Console.ResetColor();
                         break;
                 }
+            }
+        }
+        public class ParkVehicle()
+        {
+            internal void InitParking()
+            {
+                // Ask user what type of vehicle to park
+                // Ask for all info like regnum, color, numOfWheels
+                Console.WriteLine("We are initialice parking");
 
+            }
+
+
+        }
+        //Todo 
+        public class Garage<T>()
+        {
+
+        }
+        public class Vehicle
+        {
+            //Todo make read-only, porivate
+            public string RegNum { get; set; } 
+            public string Color { get; set; } 
+            public int NumOfWheels { get; set; } 
+            
+            //Todo random regnum generator
+            //Todo have 5 colors, random pick color
+            public Vehicle(string regNum = "abc123", string color="white", int numOfWheels = 4)
+            {
+                RegNum = regNum;
+                Color = color;
+                NumOfWheels = numOfWheels;
+            }
+        }
+
+        public class Airplane : Vehicle
+        {
+            public int NumOfEngines { get; set; }
+            public Airplane(string regNum, string color, int numOfWheels, int numOfEngines) : base(regNum, color, numOfWheels)  
+            {
+                NumOfEngines = numOfEngines;
+            } 
+        } 
+        public class Motorcycl : Vehicle
+        {
+            public int CylinderVolume { get; set; }
+            public Motorcycl(string regNum, string color, int numOfWheels, int cylinderVolume) : base(regNum, color, numOfWheels)  
+            {
+                CylinderVolume = cylinderVolume;
+            } 
+        }
+
+        public class Car : Vehicle
+        {
+
+            public enum FuelType { Gasoline, Diesel }
+            public FuelType Fuel { get; set; }
+
+            public Car(string regNum, string color, int numOfWheels, FuelType fuel) : base(regNum, color, numOfWheels)
+            {
+                Fuel = fuel;
+            }
+
+        }
+
+        public class Bus : Vehicle 
+        { 
+            public int NumOfSeats { get; set; }
+
+            public Bus(string regNum, string color, int numOfWheels, int numOfSeats) : base(regNum, color, numOfWheels)
+            {
+                NumOfSeats = numOfSeats;
+            }
+        }
+
+        public class Boat : Vehicle
+        {
+            public double Length { get; set; }
+
+            public Boat(string regNum, string color, int numOfWheels, double length) : base(regNum, color, numOfWheels)
+            {
+                Length = length;
             }
         }
     }
